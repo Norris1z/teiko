bceda8eb4b89f38b38fd60118fe1b4fc14048784
Norris1z <norrisjibril@gmail.com>
2018-01-30 06:43:35 +0000

    Implement a simple asm parser

--- backend/utils/asm_parser.js
+++ backend/utils/asm_parser.js
@@ -1,6 +1,30 @@
 class ASMParser{
+    constructor(){
+        this.unwantedDirectives = [
+            'file',
+            'text',
+            'def',
+            'align',
+            'set',
+            'ident',
+            'seh',
+            'section',
+            'type',
+            'scl',
+            'globl'
+        ];
+    }
+    
+    removeEmptyNewLines(asm){
+        return asm.replace(/^\s*[\r\n]/gm,'');
+    }
+
     parse(asm){
-        return asm;
+        this.unwantedDirectives.forEach((directive)=>{
+            let pattern = new RegExp(`\.${directive}.*$`,'gm')
+            asm = asm.replace(pattern,'');
+        });
+        return this.removeEmptyNewLines(asm);
     }
 }
 
